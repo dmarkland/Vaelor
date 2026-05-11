07685f20d274bce589da062f82caf156f10d2db4
dmarkland <markland2001@gmail.com>
2026-05-10 20:24:03 -0500

    Clean up favicon approach

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,10 +30,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/logo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
-      </head>
       <body className={`${lexend.variable} antialiased`} style={{ fontFamily: "var(--font-lexend), sans-serif" }}>
         {children}
       </body>
